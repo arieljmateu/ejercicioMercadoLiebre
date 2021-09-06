aad98e8c1c4cec3bcad606e099c5d3297fd9e69d
arieljmateu <arieljmateu@gmail.com>
2021-09-06 20:24:48 -0300

    procfile

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const app = express();
 
 const publicpath = path.resolve(__dirname, './public');
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static(publicpath));
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Servidor corriendo en el puerto 3000");
 });
 
